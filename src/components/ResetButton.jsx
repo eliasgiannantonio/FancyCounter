3fd8aa19a1252da609dc672db982c6c4e61c3fe7
eliasgiannantonio <giannantonioelias@gmail.com>
2024-09-10 19:26:17 -0300

    Solucion de bug de boton reset

--- src/components/ResetButton.jsx
+++ src/components/ResetButton.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { ResetIcon } from "@radix-ui/react-icons";
 
 function ResetButton({ setNumber, number }) {
-  const handleReset = () => {
+  const handleReset = (event) => {
     setNumber(number = 0);
+    event.currentTarget.blur();
   };
 
   return <button className="reset-btn" onClick={handleReset}><ResetIcon className="reset-btn-icon"/></button>;
